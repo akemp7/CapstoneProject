4218db1250d6a0dc4604c4fdb51577d515a0e4ba
Anita Kemp <anita.k.kemp@gmail.com>
2019-12-08 14:05:18 -0800

    Add input area for summary

--- ReactFrontEnd/src/components/Contribute.jsx
+++ ReactFrontEnd/src/components/Contribute.jsx
@@ -1,11 +1,20 @@
 //This will show the youtube video and have an area to input summary 
 import React from 'react';
 import YoutubeEmbedVideo from 'youtube-embed-video';
+import Navbar from './Navbar';
 
 function Contribute(){
     return(
         <div>
+            <Navbar />
             <YoutubeEmbedVideo videoId="AkLnj5pJtDI" suggestions={false} />
+
+            <form>
+                <textarea
+                id='summary'
+                placeholder='Summarize what happened in the video' />
+                <button type="submit" className="btn-btn">Submit!</button>
+            </form>
         </div>
     );
 }
