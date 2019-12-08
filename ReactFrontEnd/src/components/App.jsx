b7854c1194f9fff3f4d674b3c734e5a396c5a5a5
Anita Kemp <anita.k.kemp@gmail.com>
2019-12-08 13:29:53 -0800

    Edit routes in app

--- ReactFrontEnd/src/components/App.jsx
+++ ReactFrontEnd/src/components/App.jsx
@@ -3,19 +3,17 @@ import Home from './Home';
 import NewSearchControl from './NewSearchControl';
 import NewSearchForm from './NewSearchForm';
 import Contribute from './Contribute';
-import { Swith, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 function App(){
     return(
         <div>
+
             <Switch>
                 <Route exact path="/" component={Home} />
-                <Route path="/contribute" component={Contribute} />
-                <Route path="/search" component={NewSearchForm} />
-                <Route path="/mysearch" component={NewSearchControl} />
             </Switch>
         </div>
-    )
+    );
 }
 
 export default App;
