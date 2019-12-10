9813a743e6274659347a135bb83d516e80c18bca
Anita Kemp <anita.k.kemp@gmail.com>
2019-12-10 08:49:42 -0800

    Update CorpusApi

--- Corpus/Models/CorpusApi.cs
+++
@@ -1,9 +0,0 @@
-using System.ComponentModel.DataAnnotations;
-namespace Corpus.Models
-{
-    public class Summary
-    {
-        public int SummaryId { get; set; }
-        public string UserSumm {get; set; }
-    }
-}
